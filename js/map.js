dc18f49a2ecf473ab5f0d0bc3341e95582c79c98
Stanislav Shastov <shastov.stas1991@gmail.com>
2022-05-22 06:24:22 +0300

    Work on map and footer, form effects
    + Work on map and footer, form effects

--- js/map.js
+++ js/map.js
@@ -1,14 +1,25 @@
-ymaps.ready(init);
-    function init(){
-        // Создание карты.
-        var myMap = new ymaps.Map("map", {
-            // Координаты центра карты.
-            // Порядок по умолчанию: «широта, долгота».
-            // Чтобы не определять координаты центра карты вручную,
-            // воспользуйтесь инструментом Определение координат.
-            center: [55.76, 37.64],
-            // Уровень масштабирования. Допустимые значения:
-            // от 0 (весь мир) до 19.
-            zoom: 13
-        });
-    }
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [55.758468, 37.601088],
+            zoom: 9,
+            controls: ['zoomControl', 'geolocationControl'] 
+        }, {
+            zoomControlSize: 'small',
+            zoomControlPositionLeft: '300',
+            searchControlProvider: 'yandex#search'
+        }) 
+
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Собственный значок метки',
+            balloonContent: 'Это красивая метка'
+        }, {
+            
+            iconLayout: 'default#image',
+            iconImageHref: 'img/ellipse-map.png',
+            iconImageSize: [20, 20],
+            iconImageOffset: [-5, -38]
+        })
+    
+    myMap.geoObjects.add(myPlacemark);
+        
+});
